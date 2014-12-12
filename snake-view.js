c32e2276ad5a1a352162ae48b352804c23dfb17a
Duncan Skinner <skinnerduncan@gmail.com>
2014-12-11 16:39:33 -0800

    reduce interval to 100ms

--- snake-view.js
+++ snake-view.js
@@ -40,7 +40,7 @@
 	this.turnedThisInterval = false;
     this.bindEvents();
     this.renderBoard();
-	this.intervalId = setInterval(this.makeMove.bind(this), 75);
+	this.intervalId = setInterval(this.makeMove.bind(this), 100);
   }
   	  
   PlayView.prototype.bindEvents = function () {
