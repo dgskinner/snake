6ecf123a4ca394974d1f524fed80d8c5907cc3ee
Duncan Skinner <skinnerduncan@gmail.com>
2015-01-09 13:26:04 -0800

    slow down interval to take screenshot

--- snake-view.js
+++ snake-view.js
@@ -32,7 +32,7 @@
 	this.turnedThisInterval = false;
     this.bindEvents();
     this.renderBoard();
-	this.intervalId = setInterval(this.makeMove.bind(this), 100);
+	this.intervalId = setInterval(this.makeMove.bind(this), 700);
   }
   	  
   PlayView.prototype.bindEvents = function () {
